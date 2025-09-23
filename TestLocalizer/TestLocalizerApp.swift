7426b0ffac98e1969649f09f613257ad45816545
sebasAtWillowTree <sebastian.aguirre@willowtreeapps.com>
2025-09-23 11:27:59 -0400

    Initial Commit

---
+++ TestLocalizer/TestLocalizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLocalizerApp.swift
+//  TestLocalizer
+//
+//  Created by Sebastian Aguirre on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct TestLocalizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
